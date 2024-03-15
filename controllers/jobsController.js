f3cebfdcfc9735da2038170bb33d4c96b8ee22f3
iamamanpandey <iamamanpandey007@gmail.com>
2024-03-15 12:40:18 +0530

    added location modal

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,17 +1,17 @@
+const { json } = require("body-parser");
 const Job = require("../models/jobs");
 
 exports.getJobs = async (req, res, next) => {
   const job = await Job.find();
   res.status(200).json({
     success: true,
-    message: "Job Created",
+    result: Job.length,
     data: job,
   });
 };
 
 exports.newJobs = async (req, res, next) => {
   const job = await Job.create(req.body);
-  console.log(req.body, "reee");
   res.status(200).json({
     success: true,
     message: "Job Created",
