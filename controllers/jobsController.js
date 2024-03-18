ea641212537e5902be95edf9970514385cad43f0
iamamanpandey <iamamanpandey007@gmail.com>
2024-03-18 13:34:00 +0530

    added new route system and new controllers

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,5 +1,5 @@
-const { json } = require("body-parser");
 const Job = require("../models/jobs");
+const mongoose = require("mongoose");
 
 exports.getJobs = async (req, res, next) => {
   const job = await Job.find();
@@ -10,11 +10,78 @@ exports.getJobs = async (req, res, next) => {
   });
 };
 
+exports.getJob = async (req, res, next) => {
+  try {
+    let job;
+    const isValidObjectId = mongoose.Types.ObjectId.isValid(req?.params?.id);
+    // const job = await Job.findOne({
+    //  $and/$or: [{ _id: req.params.id }, { slug: req.params.slug }],
+    // });
+
+    if (isValidObjectId) {
+      job = await Job.findOne({ _id: req.params.id });
+    } else {
+
+      job = await Job.findOne({ slug: req.params.slug });
+      console.log(req.params.slug)
+    }
+    if (!job || job.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Job not found.",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      result: job?.length,
+      data: job,
+    });
+  } catch (error) {
+    console.log(error?.message);
+  }
+};
+
 exports.newJobs = async (req, res, next) => {
   const job = await Job.create(req.body);
   res.status(200).json({
     success: true,
     message: "Job Created",
+    data: job,
+  });
+};
+
+exports.updateJob = async (req, res) => {
+  let job = await Job.findById(req?.params?.id);
+
+  if (!job) {
+    res.status(404).json({
+      success: false,
+      message: "Job not found.",
+    });
+  }
+  job = await Job.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    success: true,
+    message: "Job has been updated",
+    data: job,
+  });
+};
+
+exports.deleteJob = async (req, res) => {
+  let job = await Job.findById(req?.params?.id);
+  if (!job) {
+    res.status(404).json({
+      success: false,
+      message: "Job not found.",
+    });
+  }
+  job = await Job.findByIdAndDelete(req.params.id);
+  res.status(200).json({
+    success: true,
+    message: "Job has been updated",
     data: job,
   });
 };
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { newJobs ,getJobs} = require("../controllers/jobsController");
+const {
+  newJobs,
+  getJobs,
+  updateJob,
+  deleteJob,
+  getJob,
+} = require("../controllers/jobsController");
 
-router.route("/jobs").get(getJobs)
-
-router.route("/job/new").post(newJobs);
+router.get("/jobs", getJobs);
+router.get("/job/:id", getJob);
+router.put("/job/:id", updateJob);
+router.delete("/job/:id", deleteJob);
+router.post("/job/new", newJobs);
 
 module.exports = router;
